c168c90520d3f69804f3c1bd09c71dce4440ef8b
jenifidelia <fideliaj@berea.edu>
2020-02-25 15:59:11 -0500

    second commit

--- app/src/main/java/com/codepath/parstagram/MainActivity.java
+++ app/src/main/java/com/codepath/parstagram/MainActivity.java
@@ -2,13 +2,50 @@ package com.codepath.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG="MainActivity";
+    private EditText etDesctiption;
+    private Button btnCaptureImage;
+    private ImageView ivPostImage;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etDesctiption= findViewById(R.id.etDescription);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        ivPostImage = findViewById(R.id.ivPostImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        
+        queryPost();
+    }
+
+    private void queryPost() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null){
+                    Log.e(TAG,"issues with getting post", e);
+                    return;
+                }
+            }
+        });
     }
 }
